5f4ced26a889327542725e32d69d9aefa8b3aecc
nick.durbin <nick.durbin@gmail.com>
2020-01-07 21:11:25 -0500

    Added a sanity check.

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.get('/', (req, res) => {
+  res.send("<h2>I am your sever and I see everything.</h2>")
+})
+
+server.use((req, res, next) => {
+  res.status(404).json({ message: "You bit off more than you can chew. Try again."})
+})
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: "Well, you caught me. You caught the Tater! We messed up."})
+})
+
 module.exports = server;
